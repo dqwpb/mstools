b0bf7cd394661f6501b0a94e5451138005b41425
dqwpb <dqwpb@users.noreply.github.com>
2016-11-08 22:58:27 +0800

    改成當前路徑吐檔案

--- displayHotfixV1.bat
+++ displayHotfixV1.bat
@@ -1,10 +1,12 @@
 @ECHO OFF
 
-SET /P POS="結果放置在: "
+REM SET /P POS="結果放置在: "
 
 REM 將主要更新(Hotfix)印出成txt檔：
 
-wmic qfe list brief /format:texttablewsys > "%POS%\hotfix.txt"
+REM wmic qfe list brief /format:texttablewsys > "%POS%\hotfix.txt"
+
+wmic qfe list brief /format:texttablewsys > .\hotfix.txt
 
 REM 將主要更新(Hotfix)印出成html檔：
 
